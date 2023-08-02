4d78a95d56c78d8e5f3104dc3d2bf608873cf1a2
Ana Izoria <ana.izoria999@gmail.com>
2023-08-02 20:51:24 +0400

    feat:setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function App() {
+  return <Main></Main>;
+}
+
+export default App;
+
+const Main = styled.main`
+  background-color: #fff;
+  width: 100%;
+  height: 100%;
+`;
